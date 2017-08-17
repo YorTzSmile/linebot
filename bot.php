e6cbcfc55d889a79c83d33eab53962e181891cb7
k.kanchanapiboon <k.kanchanapiboon@gmail.com>
2017-08-18 02:08:12 +0700

    tasdsssdwd

--- bot.php
+++ bot.php
@@ -13,7 +13,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
-			$userId = $event['source']['userId'];
+			$userId = $event['source'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
